703d3c03ea7c4c630b52ebdbde76b5cc8f78f61a
RaduProdan <raduprodan200@gmail.com>
2020-11-29 22:44:46 +0000

    change alt logo

--- components/footer.js
+++ components/footer.js
@@ -3,11 +3,7 @@ import styles from "../styles/footer.module.scss";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <img
-        src="/El_logo_colour_12.09.png"
-        alt="Vercel Logo"
-        className={styles.logo}
-      />
+      <img src="/El_logo_colour_12.09.png" alt="Logo" className={styles.logo} />
     </footer>
   );
 }
